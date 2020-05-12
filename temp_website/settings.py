1a1be6e256a70785b2993304c79da08073712f7e
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-05-12 14:16:34 +0300

    Refactor code

--- temp_website/settings.py
+++ temp_website/settings.py
@@ -100,13 +100,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'Europe/Bucharest'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
