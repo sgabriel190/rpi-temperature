1311dca0d2e4dfb289729a9eacc8d3ab02a2cd97
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-05-13 19:35:59 +0300

    button led disable

--- tempsens/urls.py
+++ tempsens/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
+    path("disable_led",views.DisableLed.as_view(), name="disable"),
 ]
--- tempsens/views.py
+++ tempsens/views.py
@@ -1,16 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.views.generic import View
-from .raspi.temp_sens import getInfo
+#from .temp_sens import getInfo, powerOffLed
 
 class Index(View):
     path = "tempsens/index.html"
 
     def get(self, request):
-        info = getInfo()
-        return render(request, self.path, { 
+        #info = getInfo()
+        return render(request, self.path)
+        """, { 
             'temperature': info["temp"],
             'humidity': info["hum"],
             'local_time': info["time"],
-         })
+         }"""
+
+class DisableLed(View):
+    def post(self, request):
+        #powerOffLed()
+        return redirect("")
