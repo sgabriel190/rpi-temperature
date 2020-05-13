7e700049c4afb83473781af23f19c20cb51a62e8
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-05-13 16:00:36 +0300

    updated settings

--- temp_website/settings.py
+++ temp_website/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
-    'django.middleware.CSPMiddleware',
+    'csp.middleware.CSPMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
