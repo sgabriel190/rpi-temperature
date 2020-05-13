d22108c987d2b006c4fbe575a2733ef2c2b11193
picolo190 <strilciuc_gabriel98@yahoo.ro>
2020-05-13 17:59:31 +0300

    updated js

--- static/tempsens/js/script.js
+++ static/tempsens/js/script.js
@@ -1,4 +1,19 @@
 function getCurrentTime() {
-    var d = new Date();
-    document.getElementById("local_time").innerHTML = "Time:" + d.toLocaleTimeString();
+    var date = new Date();
+    var hour = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    document.getElementById("local_time").innerHTML = "Time: " +
+        hour + ":" +
+        minutes + ":" +
+        seconds;
 }
