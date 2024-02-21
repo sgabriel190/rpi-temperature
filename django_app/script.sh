d5b035a10a7c1e69876f5edfb0fde3406a5fb384
Gabriel Strilciuc <strilciuc_gabriel98@yahoo.ro>
2024-02-21 21:10:53 +0200

    Remove django unnecessary scripts.

--- django_app/script.sh
+++
@@ -1,3 +0,0 @@
-sudo python3 manage.py runserver 192.168.0.107:80 &> ./logging/django_logger.log &
-celery -A temp_website worker -l info -n worker1 &> ./logging/celery_worker1_logger.log &
-celery -A temp_website worker -l info -n worker2 &> ./logging/celery_worker2_logger.log &
