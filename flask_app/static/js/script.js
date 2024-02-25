e7a0fcf39653ee8aaece03a69ad5d91db460c464
Gabriel Strilciuc <strilciuc_gabriel98@yahoo.ro>
2024-02-25 23:04:43 +0200

    Update static content.

--- flask_app/static/js/script.js
+++ flask_app/static/js/script.js
@@ -12,15 +12,20 @@ function getCurrentTime() {
     if (hour < 10) {
         hour = "0" + hour;
     }
-    document.getElementById("local_time").innerHTML = "Time: " +
-        hour + ":" +
-        minutes + ":" +
-        seconds;
+    return hour + ":" +
+        minutes + ":" + seconds;
+}
+
+function updateTime() {
+    document.getElementById("local_time").innerHTML = "Time: " + getCurrentTime();
 }
 
 function getTemperatureValue() {
-    $.post("/tempsens/get_data_json", (data, status) => {
+    $.post("/tempsens/get_data_json", (data) => {
         document.getElementById("local_temperature").innerHTML = "Temperature: " + data.temperature.toFixed(1);
         document.getElementById("local_humidity").innerHTML = "Humidity: " + data.humidity.toFixed(1);
+        document.getElementById("sensor_status").innerHTML = "Updated with sensor data at " + getCurrentTime();
+    }).fail(() => {
+        document.getElementById("sensor_status").innerHTML = "FAILED to update with sensor data at " + getCurrentTime();
     });
 }
